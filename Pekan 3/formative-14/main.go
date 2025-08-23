7e760a5cad349742a474a7147e5b308f75be3bfc
Luthfifasya <luthfi.fasya29@gmail.com>
2025-08-23 10:53:53 +0700

    mengganti password dsn

--- Pekan 3/formative-14/main.go
+++ Pekan 3/formative-14/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func initDB() *gorm.DB {
-	dsn := "host=localhost user=postgres password=YOUR_PASSWORD dbname=bioskop_db port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=lightyagami321 dbname=bioskop_db port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Gagal koneksi database: %v", err)
